2024d7468d80ec7d9c46fc8f9d1f62c438d80757
DhanushkumarSiv <ds9035697@gmail.com>
2026-06-04 17:11:45 +0530

    Test Commit1

--- src/test/java/com/project/hostel_management/service/AttendanceNetworkAccessServiceTest.java
+++ src/test/java/com/project/hostel_management/service/AttendanceNetworkAccessServiceTest.java
@@ -16,9 +16,9 @@ class AttendanceNetworkAccessServiceTest {
     void allowsHostelNetworkIp() {
         AttendanceNetworkAccessService service = serviceWithDefaults();
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setRemoteAddr("152.57.83.236");
+        request.setRemoteAddr("172.68.146.246");
 
-        assertEquals("152.57.83.236", service.validateAttendanceMarkingAccess(request));
+        assertEquals("172.68.146.246", service.validateAttendanceMarkingAccess(request));
     }
 
     @Test
@@ -41,10 +41,10 @@ class AttendanceNetworkAccessServiceTest {
         AttendanceNetworkAccessService service = serviceWithDefaults();
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setRemoteAddr("127.0.0.1");
-        request.addHeader("X-Forwarded-For", "152.57.83.236, 127.0.0.1");
+        request.addHeader("X-Forwarded-For", "172.68.146.246, 127.0.0.1");
 
-        assertEquals("152.57.83.236", service.resolveClientIp(request));
-        assertEquals("152.57.83.236", service.validateAttendanceMarkingAccess(request));
+        assertEquals("172.68.146.246", service.resolveClientIp(request));
+        assertEquals("172.68.146.246", service.validateAttendanceMarkingAccess(request));
     }
 
     @Test
@@ -52,7 +52,7 @@ class AttendanceNetworkAccessServiceTest {
         AttendanceNetworkAccessService service = serviceWithDefaults();
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setRemoteAddr("192.168.1.45");
-        request.addHeader("X-Forwarded-For", "152.57.83.236");
+        request.addHeader("X-Forwarded-For", "172.68.146.246");
 
         assertEquals("192.168.1.45", service.resolveClientIp(request));
         assertThrows(
@@ -63,7 +63,7 @@ class AttendanceNetworkAccessServiceTest {
 
     private AttendanceNetworkAccessService serviceWithDefaults() {
         AttendanceNetworkProperties properties = new AttendanceNetworkProperties();
-        properties.setAllowedSubnets(List.of("152.57.0.0-152.57.255.255"));
+        properties.setAllowedSubnets(List.of("172.68.0.0-172.68.255.255"));
         properties.setTrustedProxySubnets(List.of("127.0.0.1/32"));
         properties.setAllowLocalhost(true);
         properties.setTrustProxyHeaders(true);
--- src/test/java/com/project/hostel_management/util/IpAddressUtilTest.java
+++ src/test/java/com/project/hostel_management/util/IpAddressUtilTest.java
@@ -11,19 +11,19 @@ class IpAddressUtilTest {
 
     @Test
     void allowsIpv4InsideConfiguredRange() {
-        List<String> allowedRules = List.of("152.57.0.0-152.57.255.255");
+        List<String> allowedRules = List.of("172.68.0.0-172.68.255.255");
 
-        assertTrue(IpAddressUtil.isAllowedIpv4("152.57.0.0", allowedRules));
-        assertTrue(IpAddressUtil.isAllowedIpv4("152.57.83.236", allowedRules));
-        assertTrue(IpAddressUtil.isAllowedIpv4("152.57.255.255", allowedRules));
+        assertTrue(IpAddressUtil.isAllowedIpv4("172.68.0.0", allowedRules));
+        assertTrue(IpAddressUtil.isAllowedIpv4("172.68.146.246", allowedRules));
+        assertTrue(IpAddressUtil.isAllowedIpv4("172.68.255.255", allowedRules));
     }
 
     @Test
     void rejectsIpv4OutsideConfiguredRange() {
-        List<String> allowedRules = List.of("152.57.0.0-152.57.255.255");
+        List<String> allowedRules = List.of("172.68.0.0-172.68.255.255");
 
-        assertFalse(IpAddressUtil.isAllowedIpv4("152.56.255.255", allowedRules));
-        assertFalse(IpAddressUtil.isAllowedIpv4("152.58.0.0", allowedRules));
+        assertFalse(IpAddressUtil.isAllowedIpv4("172.67.255.255", allowedRules));
+        assertFalse(IpAddressUtil.isAllowedIpv4("172.69.0.0", allowedRules));
     }
 
     @Test
